3a2b9f803ae80a177abaa50743a360d3823a29a1
AneWN <aneweienilsen@gmail.com>
2023-09-23 22:09:35 +0200

    instagram

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ const Home: NextPage = () => {
   return (
     <App
       linkedIn="https://www.linkedin.com/in/caspar-ramstad-heide-46262252/"
+      instagram="https://instagram.com/casparheide?igshid=MzRlODBiNWFlZA=="
       email="casparheide@gmail.com"
       image={<Image src={image} alt="Caspar Ramstad Heide" />}
       profile={profile}
