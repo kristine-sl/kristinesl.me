f7c09490e698846bd3c787f638b6a59822d59a92
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2023-09-24 02:13:06 +0200

    fix: remove gallery to implement in seperate branch

--- assets/data/index.ts
+++ assets/data/index.ts
@@ -1,9 +1,8 @@
 import education from './education.json';
 import experience from './experience.json';
-import gallery from './gallery.json';
 import lookingFor from './lookingFor.json';
 import other from './other.json';
 import profile from './profile.json';
 import research from './research.json';
 
-export { education, experience, gallery, lookingFor, other, profile, research };
+export { education, experience, lookingFor, other, profile, research };
